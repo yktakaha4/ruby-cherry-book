6cbf6cf4f8b47a79302a3b25c366ab6d4502bc07
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-12-23 18:17:54 +0900

    add effects

--- lib/effects.rb
+++ lib/effects.rb
@@ -1,4 +1,19 @@
 module Effects
   def self.reverse
+    ->(words) do
+      words.split(' ').map(&:reverse).join(' ')
+    end
+  end
+
+  def self.echo(rate)
+    ->(words) do
+      words.each_char.map { |char| char == ' ' ? char : char * rate }.join
+    end
+  end
+
+  def self.loud(level)
+    ->(words) do
+      words.split(' ').map { |word| word.upcase + '!' * level }.join(' ')
+    end
   end
 end
